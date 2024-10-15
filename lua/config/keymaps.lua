f81aaf47211549e2f91583827f28f813c3cd97c6
eason-zhan <easonzhan.hj@gmail.com>
2024-10-15 08:15:24 +0800

    update my custom keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleaer = ","
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>q", ":q<CR>") -- quit
+keymap.set("n", "<leader>s", ":w<CR>") -- save
+keymap.set("n", "-", "ddp") -- Swap current line with next line
+keymap.set("n", "_", "ddkP") -- Swap current line with prev line
