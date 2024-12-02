0c1a987d388d881a18bfe49188dee5b1a4639a54
eason-zhan <easonzhan.hj@gmail.com>
2024-12-02 11:02:18 +0800

    add windows and surrond lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,9 @@
 local keymap = vim.keymap
 
+local function cmd(command)
+  return table.concat({'<cmd>', command, '<CR>'})
+end
+
 -- General keymaps
 keymap.set('n', '<leader>q', ':q<CR>', {desc = 'Quit without saving'})
 keymap.set('n', '<leader>w', ':w<CR>', {desc = 'Save file'})
@@ -17,4 +21,11 @@ keymap.set('n', '<leader>p', fzf.files, { desc = 'Fzf Files'})
 keymap.set('n', '<leader>b', fzf.buffers, { desc = 'Fzf Buffers'})
 keymap.set('n', '<leader>,', fzf.commands, { desc = 'Fzf Commands'})
 keymap.set('n', '<leader>ag', fzf.grep_cWORD, { desc = 'Fzf Search WORD under cursor'}) -- maybe base is rg 
+
+
+-- Windows maximizer
+keymap.set('n', '<C-w>z', cmd 'WindowsMaximize', {desc = 'Toggle Maximize'})
+keymap.set('n', '<C-w>_', cmd 'WindowsMaximizeVertically')
+keymap.set('n', '<C-w>|', cmd 'WindowsMaximizeHorizontally')
+keymap.set('n', '<C-w>=', cmd 'WindowsEqualize')
 
