7684531ab0622a2943d66b892865bb5267ed8a59
eason-zhan <easonzhan.hj@gmail.com>
2025-05-09 13:56:30 +0800

    make lusls aware of vim

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -13,4 +13,22 @@ return {
   on_init = function()
     print('luals now runs in the background')
   end,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    }
+  }
 }
